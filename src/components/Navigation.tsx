b04a576957f2445eb95c7efcd1264ddf32d74d64
ShatterPlayer <shatterplayer@gmail.com>
2022-07-04 22:33:35 +0200

    Finished navigation overall appearance

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { DefaultTheme } from 'styled-components'
 
 const Nav = styled.nav`
   display: flex;
@@ -8,40 +8,137 @@ const Nav = styled.nav`
   position: fixed;
   top: 0;
   left: 0;
-  background-color: rgb(0, 0, 0, 0.2);
   width: 100%;
   height: 70px;
   z-index: 99;
+  backdrop-filter: blur(5px);
+  background-color: rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.5);
+`
+const MenuContainer = styled.div`
+  position: relative;
 `
 
-const List = styled.ul`
+const menuItemWidth = 120
+
+const Menu = styled.ul<MenuProps>`
   list-style: none;
   display: flex;
   align-items: center;
+  width: 80vw;
+
+  ${({ borderhidden, theme, bordercolor }) =>
+    `
+    & ${MenuItem},
+    & ${MenuItemsJoin} {
+      border-color: ${
+        borderhidden ? 'transparent' : theme.colors[bordercolor ?? 'light']
+      };
+    }
+  `}
 `
 
-const ListItem = styled.li`
-  padding: 10px;
-  border: 5px solid white;
+const MenuOutline = styled(Menu)<MenuProps>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -2;
+`
+
+const MenuItem = styled.a`
+  width: ${menuItemWidth}px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 5px solid ${({ theme }) => theme.colors.light};
   border-radius: 30px;
+  cursor: pointer;
+  text-decoration: none;
+  color: inherit;
 `
 
-const ListItemsJoin = styled.li`
-  width: 100px;
+const MenuItemsJoin = styled.li`
+  width: calc((100% - 3 * ${menuItemWidth}px) / 4);
   height: 5px;
-  border-bottom: 5px solid white;
+  border-bottom: 5px solid ${({ theme }) => theme.colors.light};
 `
+
+const ProgressOutline = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  height: 100%;
+  width: calc(50% + ${menuItemWidth}px + (100% - 3 * ${menuItemWidth}px) / 4);
+  z-index: -1;
+
+  & ${Menu} {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`
+
+interface MenuProps {
+  bordercolor?: keyof DefaultTheme['colors']
+  borderhidden?: boolean
+}
 
 function Navigation() {
   return (
     <Nav>
-      <List>
-        <ListItem>About me</ListItem>
-        <ListItemsJoin />
-        <ListItem>Projects</ListItem>
-        <ListItemsJoin />
-        <ListItem>Contact</ListItem>
-      </List>
+      <MenuContainer>
+        <Menu borderhidden>
+          <MenuItemsJoin />
+          <li>
+            <MenuItem href="#">About me</MenuItem>
+          </li>
+          <MenuItemsJoin />
+          <li>
+            <MenuItem href="#">Projects</MenuItem>
+          </li>
+          <MenuItemsJoin />
+          <li>
+            <MenuItem href="#">Contact</MenuItem>
+          </li>
+          <MenuItemsJoin />
+        </Menu>
+
+        <MenuOutline bordercolor="light">
+          <MenuItemsJoin />
+          <li>
+            <MenuItem as="span" />
+          </li>
+          <MenuItemsJoin />
+          <li>
+            <MenuItem as="span" />
+          </li>
+          <MenuItemsJoin />
+          <li>
+            <MenuItem as="span" />
+          </li>
+          <MenuItemsJoin />
+        </MenuOutline>
+
+        <ProgressOutline>
+          <Menu bordercolor="yellow">
+            <MenuItemsJoin />
+            <li>
+              <MenuItem as="span" />
+            </li>
+            <MenuItemsJoin />
+            <li>
+              <MenuItem as="span" />
+            </li>
+            <MenuItemsJoin />
+            <li>
+              <MenuItem as="span" />
+            </li>
+            <MenuItemsJoin />
+          </Menu>
+        </ProgressOutline>
+      </MenuContainer>
     </Nav>
   )
 }
