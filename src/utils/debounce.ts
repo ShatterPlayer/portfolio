75d9342d0ed0ff8b8a0c934e86885d56d879c3c6
Michal Karpierz <michalk101010@gmail.com>
2023-02-08 11:59:36 +0100

    Corrected types for debounce function

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,5 +1,5 @@
 export default function debounce(fn: Function, wait: number) {
-  let timeout: any
+  let timeout: ReturnType<typeof setTimeout>
   return (...args: any[]) => {
     clearTimeout(timeout)
     timeout = setTimeout(() => fn(...args), wait)
